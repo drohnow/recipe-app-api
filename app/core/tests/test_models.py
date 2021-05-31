5f40a1a0102687b608742e88025b8a05338f910f
drohnow <davidrohnow@gmail.com>
2021-05-31 16:21:40 -0700

    fixes moadels

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,7 +17,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """test the email for a new user is normalizaed"""
-        emal = 'test@rohnow.net'
+        email = 'test@rohnow.net'
         user = get_user_model().objects.create_user(email, 'test123')
 
         self.assertEqual(user.email, email.lower())
